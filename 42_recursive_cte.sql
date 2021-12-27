c5ba78539b695d9da04fbaff147a70af3afc7a3f
Nikhil Meghnani <nikhilcse91@gmail.com>
2021-12-27 19:37:39 +0530

    Update 42_recursive_cte.sql

--- 42_recursive_cte.sql
+++ 42_recursive_cte.sql
@@ -12,13 +12,6 @@
 -- 4	4	Orange	2	3
 -- 5	5	Orange	3	3
 
-Create Table Item(
-ItemName Varchar(30),
-TotalQuantity int
-)
-Insert into Ite m Values('Apple',2)
-Insert into Item Values('Orange',3)
-
 with cte_a(itemname, catid, totalquantity) as
 (
 select itemname, 1, totalquantity from item
@@ -29,4 +22,13 @@ where catid + 1 <= TotalQuantity
 --select * from cte_a
 select ROW_NUMBER() OVER (ORDER BY ItemName) AS id, ItemName, catid, TotalQuantity from cte_A
 order by ItemName; 
+
+Create Table Item(
+ItemName Varchar(30),
+TotalQuantity int
+)
+Insert into Ite m Values('Apple',2)
+Insert into Item Values('Orange',3)
+
+
 
