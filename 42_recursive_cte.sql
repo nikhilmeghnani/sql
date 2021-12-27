cfc0de9fd89edbfab7d7504f8b966f5cf5ebe0d7
Nikhil Meghnani <nikhilcse91@gmail.com>
2021-12-27 11:02:03 +0530

    Update 42_recursive_cte.sql

--- 42_recursive_cte.sql
+++ 42_recursive_cte.sql
@@ -1,3 +1,17 @@
+-- ITEM Table  has two columns namely ItemName and TotalQuantity. 
+-- Write a SQL query  to duplicate the rows based on total count in output table by adding two new columns ID and CatID
+
+-- #	ItemName	TotalQuantity
+-- 1	Apple	2
+-- 2	Orange	3
+
+-- #	id	itemname	catid	totalquantity
+-- 1	1	Apple	1	2
+-- 2	2	Apple	2	2
+-- 3	3	Orange	1	3
+-- 4	4	Orange	2	3
+-- 5	5	Orange	3	3
+
 Create Table Item(
 ItemName Varchar(30),
 TotalQuantity int
@@ -15,3 +29,4 @@ where catid + 1 <= TotalQuantity
 --select * from cte_a
 select ROW_NUMBER() OVER (ORDER BY ItemName) AS id, ItemName, catid, TotalQuantity from cte_A
 order by ItemName; 
+
